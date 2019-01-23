9c5ff3e785df879e181d7a089bb44e85bd07b4cf
Elmer Urrea <elmer7186@gmail.com>
2019-01-22 22:49:30 -0500

    first commit

---
+++ tienda-tecnologia/src/main/java/dominio/Vendedor.java
@@ -0,0 +1,30 @@
+package dominio;
+
+import dominio.repositorio.RepositorioProducto;
+import dominio.repositorio.RepositorioGarantiaExtendida;
+
+public class Vendedor {
+
+	public static final String EL_PRODUCTO_TIENE_GARANTIA = "El producto ya cuenta con una garantia extendida";
+
+	private RepositorioProducto repositorioProducto;
+	private RepositorioGarantiaExtendida repositorioGarantia;
+
+	public Vendedor(RepositorioProducto repositorioProducto, RepositorioGarantiaExtendida repositorioGarantia) {
+		this.repositorioProducto = repositorioProducto;
+		this.repositorioGarantia = repositorioGarantia;
+
+	}
+
+	public void generarGarantia(String codigo) {
+
+		throw new UnsupportedOperationException("Método pendiente por implementar");
+
+	}
+
+	public boolean tieneGarantia(String codigo) {
+		Producto producto = repositorioGarantia.obtenerProductoConGarantiaPorCodigo(codigo);
+		return producto != null;
+	}
+
+}
